269ed2f98759128cfe9ddd2a2f5b3a028c012f3b
rangamach <117797904+rangamach@users.noreply.github.com>
2024-11-13 17:11:59 +0530

    Crouch Animation Working.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class PlayerController : MonoBehaviour
 {
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Debug.Log("Collided with " +  collision.gameObject.name);
-    //}
-
+    private bool can_jump = false;
+    private bool can_crouch = false;
     public Animator animator;
 
     private void Update()
     {
+        //running left or right
+        HoriontalMovement();
+        //jumping or crouching
+        VerticalMovement();
+    }
+
+    private void VerticalMovement()
+    {
+        float speed = Input.GetAxisRaw("Vertical");
+        animator.SetBool("Jump", can_jump);
+        animator.SetBool("Crouch", can_crouch);
+        if (speed > 0 && !can_jump)
+        {
+            can_jump = true;
+        }
+        else if (speed < 0 && !can_crouch)
+        {
+            can_crouch= true;
+        }
+        else if(speed == 0)
+        {
+            if(can_jump)
+                can_jump = false;
+            if(can_crouch)
+                can_crouch = false;
+        }
+    }
+
+    private void HoriontalMovement()
+    {
         float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        animator.SetFloat("HorizontalSpeed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {
             scale.x = Mathf.Abs(scale.x) * -1;
         }
-        else
+        else if (speed > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
